bc896da62db467c706d43c5710df0f99d6b86f68
James Alderman <james_codes@icloud.com>
2024-12-06 16:28:06 +0000

    background image to build added

--- parker-landing-page/src/App.js
+++ parker-landing-page/src/App.js
@@ -9,7 +9,10 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div
+      className="min-h-screen bg-gray-100 bg-no-repeat bg-right-top"
+      style={{ backgroundImage: "url('/hex-background.png')" }}
+    >
       <main className="container mx-auto px-4 py-6 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1536px]">
         <TopNav />
         <ImageAside />
