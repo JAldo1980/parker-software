aca1f37eb3ce1ff0d5aeea7523535312724946df
James Alderman <james_codes@icloud.com>
2024-12-06 11:12:28 +0000

    added container parameters for tailwind via tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,26 @@
 // }
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      container: {
+        center: true, // Centers the container by default
+        padding: "2rem", // Adds padding to the container
+        screens: {
+          sm: "100%", // sm: 100% width
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+        },
+      },
+    },
   },
-  plugins: [],
 };
