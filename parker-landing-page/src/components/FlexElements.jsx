a8956cf512627c61fa8448d05fdde1e7cec5082a
James Alderman <james_codes@icloud.com>
2024-12-06 16:31:24 +0000

    FlexElement bg colour update

--- parker-landing-page/src/components/FlexElements.jsx
+++ parker-landing-page/src/components/FlexElements.jsx
@@ -5,26 +5,26 @@ function FlexElements() {
     <div className="flex flex-col items-center justify-center pt-6">
       {/* Top Row */}
       <div className="flex gap-4 mb-10">
-        <div className="flex justify-center w-96 h-20 bg-blue-400 p-2 align-center">
+        <div className="flex justify-center w-96 h-20 bg-[#9f61db] p-2 align-center">
           <h3>Problem goes here</h3>
         </div>
-        <div className="flex flex-wrap justify-center w-120 h-20 bg-blue-400 p-2 align-center">
+        <div className="flex flex-wrap justify-center w-120 h-20 bg-[#9f61db] p-2 align-center">
           <h3>Problem goes here</h3>
         </div>
-        <div className="flex justify-center w-56 h-20 bg-blue-400 p-2 align-center">
+        <div className="flex justify-center w-56 h-20 bg-[#9f61db] p-2 align-center">
           <h3>Problem goes here</h3>
         </div>
       </div>
 
       {/* Bottom Row */}
       <div className="flex gap-4 mb-10">
-        <div className="flex justify-center w-96 h-20 bg-blue-400 p-2 align-center">
+        <div className="flex justify-center w-96 h-20 bg-[#9f61db] p-2 align-center">
           <h3>Problem goes here</h3>
         </div>
-        <div className="flex flex-wrap justify-center w-120 h-20 bg-blue-400 p-2 align-center">
+        <div className="flex flex-wrap justify-center w-120 h-20 bg-[#9f61db] p-2 align-center">
           <h3>Problem goes here</h3>
         </div>
-        <div className="flex justify-center w-56 h-20 bg-blue-400 p-2 align-center">
+        <div className="flex justify-center w-56 h-20 bg-[#9f61db] p-2 align-center">
           <h3>Problem goes here</h3>
         </div>
       </div>
