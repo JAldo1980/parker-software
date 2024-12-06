d4c94e7e6c6442e5c2179ca8b36f64738310773a
James Alderman <james_codes@icloud.com>
2024-12-06 16:18:33 +0000

    gradient added to cardsProduct

--- parker-landing-page/src/components/CardsProduct.jsx
+++ parker-landing-page/src/components/CardsProduct.jsx
@@ -3,36 +3,36 @@ import React from "react";
 function CardsProduct() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-      <div className="border-2 border-black p-4 rounded-md shadow-md">
-        <h3 className="text-xl font-bold mb-2">Header here</h3>
-        <p className="mb-4">
+      <div className="p-4 rounded-md shadow-md bg-gradient-to-r from-[#228658] to-[#1b6a47]">
+        <h3 className="text-xl font-bold mb-2 text-white">Header here</h3>
+        <p className="mb-4 text-white">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
           assumenda officiis eum deserunt provident error ipsa ratione mollitia
           velit non.
         </p>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        <button className="bg-white text-[#228658] px-4 py-2 rounded hover:bg-gray-200">
           CTA here
         </button>
       </div>
-      <div className="border-2 border-black p-4 rounded-md shadow-md">
-        <h3 className="text-xl font-bold mb-2">Header here</h3>
-        <p className="mb-4">
+      <div className="p-4 rounded-md shadow-md bg-gradient-to-r from-[#228658] to-[#1b6a47]">
+        <h3 className="text-xl font-bold mb-2 text-white">Header here</h3>
+        <p className="mb-4 text-white">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
           assumenda officiis eum deserunt provident error ipsa ratione mollitia
           velit non.
         </p>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        <button className="bg-white text-[#228658] px-4 py-2 rounded hover:bg-gray-200">
           CTA here
         </button>
       </div>
-      <div className="border-2 border-black p-4 rounded-md shadow-md">
-        <h3 className="text-xl font-bold mb-2">Header here</h3>
-        <p className="mb-4">
+      <div className="p-4 rounded-md shadow-md bg-gradient-to-r from-[#228658] to-[#1b6a47]">
+        <h3 className="text-xl font-bold mb-2 text-white">Header here</h3>
+        <p className="mb-4 text-white">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
           assumenda officiis eum deserunt provident error ipsa ratione mollitia
           velit non.
         </p>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        <button className="bg-white text-[#228658] px-4 py-2 rounded hover:bg-gray-200">
           CTA here
         </button>
       </div>
