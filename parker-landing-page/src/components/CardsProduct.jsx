970418bf407319eaa91e28af93ffb8b487598f9b
James Alderman <james_codes@icloud.com>
2024-12-07 16:37:05 +0000

    update CardsProduct component with absolute positioned image/div for product logo - also updated solutions data with image url

--- parker-landing-page/src/components/CardsProduct.jsx
+++ parker-landing-page/src/components/CardsProduct.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import SolutionData, { solutions } from "../data/solutions";
+import SolutionData from "../data/solutions";
 
 function CardsProduct() {
   return (
     <>
-      <div id="solutions" className="flex justify-center mt-6">
-        <h className="sm:text-3xl md:text-3xl lg:text-4xl">Our Solutions</h>
-      </div>
+      <div id="solutions" className="flex justify-center mt-12"></div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         {SolutionData.map((card) => (
           <div
             key={card.id}
-            className="p-4 rounded-md shadow-md bg-gradient-to-r from-[#228658] to-[#1b6a47]"
+            className="mt-12 relative p-4 rounded-md shadow-md bg-gradient-to-r from-[#228658] to-[#1b6a47]"
           >
+            {/* Green circle wrapper */}
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-32 h-32 bg-blue-900 rounded-full flex items-center justify-center mb-4 p-2">
+              <img
+                src={card.thumbnail}
+                alt={card.imageAltText}
+                className="object-fit"
+              />
+            </div>
+
             <img
               src={card.image}
               alt={card.imageAltText}
--- parker-landing-page/src/data/solutions.jsx
+++ parker-landing-page/src/data/solutions.jsx
@@ -4,7 +4,7 @@ export const SolutionData = [
     header: "WhosOn",
     image: "/blog-thubnail-1.png",
     thumbnail: "/whoson-logo-sm-white.png",
-    imageAltText: "alt text here",
+    imageAltText: "WhosOn logo",
     subText:
       "WhosOn is a live chat tool that helps your team connect with customers instantly. It’s packed with smart features like directing chats to the right person, working seamlessly with your in-house systems, and even using chatbots to handle simple queries. Built with security at its core, it ensures your customer data stays private. WhosOn makes it easy to deliver great customer service while improving communication across your business.",
     cta: "Learn More",
@@ -14,7 +14,7 @@ export const SolutionData = [
     header: "ThinkAutomation",
     image: "/blog-thubnail-1.png",
     thumbnail: "/thinkautomation-white-logo.png",
-    imageAltText: "alt text here",
+    imageAltText: "ThinkAutomation logo",
     subText:
       "ThinkAutomation is like your personal assistant for managing messages. It handles emails, texts, and other incoming messages, organizing them and ensuring the right action is taken automatically. Whether it’s replying to customers, updating records, or following up on tasks, it saves you time and effort, helping your team focus on what matters most.",
     cta: "Learn More",
@@ -24,7 +24,7 @@ export const SolutionData = [
     header: "OptimaGPT",
     image: "/blog-thubnail-1.png",
     thumbnail: "/OGPT-white-logo.png",
-    imageAltText: "alt text here",
+    imageAltText: "OptimaGPT logo",
     subText:
       "OptimaGPT is a secure AI tool that runs entirely on your company’s systems, ensuring your data stays private. It lets you use AI-powered solutions, like chatbots and automated tasks, without relying on external servers. It’s cost-effective and integrates well with other tools like WhosOn and ThinkAutomation, making your workflow smarter and more efficient.",
     cta: "Learn More",
