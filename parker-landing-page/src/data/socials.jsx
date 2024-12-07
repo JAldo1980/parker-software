a36c5c9a1a2dc05ac1aafe3cd5de353bbdc99331
James Alderman <james_codes@icloud.com>
2024-12-07 13:56:58 +0000

    added new component called FlexImages to be a re-useable component. also added Alt Text to both datasets - clients and socials

---
+++ parker-landing-page/src/data/socials.jsx
@@ -0,0 +1,34 @@
+export const SocialData = [
+  {
+    id: 1,
+    img: "",
+    imgAlt: "",
+  },
+  {
+    id: 2,
+    img: "",
+    imgAlt: "",
+  },
+  {
+    id: 3,
+    img: "",
+    imgAlt: "",
+  },
+  {
+    id: 4,
+    img: "",
+    imgAlt: "",
+  },
+  {
+    id: 5,
+    img: "",
+    imgAlt: "",
+  },
+  {
+    id: 6,
+    img: "",
+    imgAlt: "",
+  },
+];
+
+export default SocialData;
