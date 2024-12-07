78c3333d36da129af932746a3b5d57fbede27fa6
James Alderman <james_codes@icloud.com>
2024-12-07 12:36:14 +0000

    client data file added

---
+++ parker-landing-page/src/data/clients.jsx
@@ -0,0 +1,36 @@
+export const ClientData = [
+  {
+    id: 1,
+    img: "/public/aviva_logo.png",
+  },
+  {
+    id: 2,
+    img: "/public/cisco_logo.png",
+  },
+  {
+    id: 3,
+    img: "/public/gsk_logo.png",
+  },
+  {
+    id: 4,
+    img: "/public/ford_logo.png",
+  },
+  {
+    id: 5,
+    img: "/public/mcdonalds_logo.png",
+  },
+  {
+    id: 6,
+    img: "/public/nasa_logo.png",
+  },
+  {
+    id: 6,
+    img: "/public/hp_logo.png",
+  },
+  {
+    id: 8,
+    img: "/public/game_logo.png",
+  },
+];
+
+export default ClientData;
