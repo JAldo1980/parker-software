2ea9627907e989b00bc50b06e70de18403c2c39d
James Alderman <james_codes@icloud.com>
2024-12-08 10:43:57 +0000

    updated CTA text in solution data file

--- parker-landing-page/src/data/solutions.jsx
+++ parker-landing-page/src/data/solutions.jsx
@@ -8,7 +8,7 @@ export const SolutionData = [
     imageAltText: "WhosOn logo",
     subText:
       "WhosOn is a live chat tool that helps your team connect with customers instantly. It’s packed with smart features like directing chats to the right person, working seamlessly with your in-house systems, and even using chatbots to handle simple queries. Built with security at its core, it ensures your customer data stays private. WhosOn makes it easy to deliver great customer service while improving communication across your business.",
-    cta: "Learn More",
+    cta: "Explore Product",
   },
   {
     id: 1,
@@ -19,7 +19,7 @@ export const SolutionData = [
     imageAltText: "ThinkAutomation logo",
     subText:
       "ThinkAutomation is like your personal assistant for managing messages. It handles emails, texts, and other incoming messages, organizing them and ensuring the right action is taken automatically. Whether it’s replying to customers, updating records, or following up on tasks, it saves you time and effort, helping your team focus on what matters most.",
-    cta: "Learn More",
+    cta: "Explore Product",
   },
   {
     id: 1,
@@ -30,7 +30,7 @@ export const SolutionData = [
     imageAltText: "OptimaGPT logo",
     subText:
       "OptimaGPT is a secure AI tool that runs entirely on your company’s systems, ensuring your data stays private. It lets you use AI-powered solutions, like chatbots and automated tasks, without relying on external servers. It’s cost-effective and integrates well with other tools like WhosOn and ThinkAutomation, making your workflow smarter and more efficient.",
-    cta: "Learn More",
+    cta: "Explore Product",
   },
 ];
 
