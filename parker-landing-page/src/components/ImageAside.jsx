a73606bc118344f0dbafeab93127111c876fd781
James Alderman <james_codes@icloud.com>
2024-12-10 15:22:27 +0000

    aria label added

--- parker-landing-page/src/components/ImageAside.jsx
+++ parker-landing-page/src/components/ImageAside.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function ImageAside() {
   return (
     <div className="grid gap-6 mt-6 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1">
-      {/* Left Section (Headline) */}
       <div className="self-center md:w-3/4 lg:w-full">
         <h2 className="text-[#3a3a3a] text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
           Enterprise Communication & Automation Solutions
@@ -12,17 +11,18 @@ function ImageAside() {
           Where innovation meets efficiency. Empowering seamless communication &
           automation for over 22 years.
         </p>
-        <button className="px-4 py-2 bg-[#e06a61] text-white font-semibold rounded-full hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+        <button
+          className="px-4 py-2 bg-[#e06a61] text-white font-semibold rounded-full hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+          aria-label="call to action button"
+        >
           Explore
         </button>
       </div>
-
-      {/* Right Section (Image) */}
       <div className="flex justify-center items-center relative w-full h-full">
         <img
           src="hero-img.png"
-          alt=""
-          className="w-2/3 md:w-1/2 lg:w-3/4" // Adjust size here
+          alt="image of computer with Parker Software products on screen "
+          className="w-2/3 md:w-1/2 lg:w-3/4"
         />
       </div>
     </div>
