8faf1ff9b0a91152b0df43fd07678a9a5fc3cec3
James Alderman <james_codes@icloud.com>
2024-12-10 14:09:04 +0000

    added ARIA elements to the TopNav component

--- parker-landing-page/src/components/TopNav.jsx
+++ parker-landing-page/src/components/TopNav.jsx
@@ -14,7 +14,11 @@ function TopNav() {
     <div className="sticky top-0 z-50 w-full bg-[#f2f2f2] shadow-lg">
       <div className=" w-full">
         <BannerStretch />
-        <div className="container mx-auto px-4 py-6 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1536px] flex justify-between items-center cursor-pointer p-4">
+        <nav
+          className="container mx-auto px-4 py-6 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1536px] flex justify-between items-center cursor-pointer p-4"
+          role="navigation"
+          aria-label="Main Navigation"
+        >
           <div>
             <img
               src="parker-software-logo.svg"
@@ -24,23 +28,41 @@ function TopNav() {
           </div>
 
           <div className="sm:hidden">
-            <button onClick={toggleMenu} className="text-xl">
+            <button
+              onClick={toggleMenu}
+              className="text-xl"
+              aria-expanded={isMenuOpen}
+              aria-controls="mobile-menu"
+              aria-label="Toggle mobile menu"
+            >
               <span className="block w-6 h-0.5 bg-black mb-1"></span>
               <span className="block w-6 h-0.5 bg-black mb-1"></span>
               <span className="block w-6 h-0.5 bg-black"></span>
             </button>
           </div>
           <div className="hidden sm:flex gap-6 items-center">
-            <a href="#solutions" className="text-[#3a3a3a] hover:underline">
+            <a
+              href="#solutions"
+              className="text-[#3a3a3a] hover:underline"
+              role="menuitem"
+            >
               Solutions
             </a>
-            <a href="#resources" className=" text-[#3a3a3a] hover:underline">
+            <a
+              href="#resources"
+              className=" text-[#3a3a3a] hover:underline"
+              role="menuitem"
+            >
               Resources
             </a>
-            <button className="px-4 py-2 bg-[#e06a61] text-white font-semibold rounded-full hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+            <button
+              className="px-4 py-2 bg-[#e06a61] text-white font-semibold rounded-full hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+              aria-label="Contact us"
+            >
               Contact
             </button>
           </div>
+          {/* start of mobile menu */}
           <div
             className={`sm:hidden absolute top-0 right-0 w-3/4 bg-white p-6 mt-14 rounded-lg shadow-md ${
               isMenuOpen ? "block" : "hidden"
@@ -50,6 +72,7 @@ function TopNav() {
               href="#solutions"
               onClick={closeMenu}
               className="text-right block mb-4 hover:underline"
+              role="menuitem"
             >
               Solutions
             </a>
@@ -57,6 +80,7 @@ function TopNav() {
               href="#resources"
               onClick={closeMenu}
               className=" text-right block mb-4 hover:underline"
+              role="menuitem"
             >
               Resources
             </a>
@@ -64,12 +88,14 @@ function TopNav() {
               <button
                 onClick={closeMenu}
                 className="text-right block px-4 py-2 bg-[#e06a61] text-white font-semibold rounded-full hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+                role="menuitem"
+                aria-label="Contact us"
               >
                 Contact
               </button>
             </div>
           </div>
-        </div>
+        </nav>
       </div>
     </div>
   );
